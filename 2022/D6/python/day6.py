94b6dfc8e8a80fb879ad7072c596f88b3c1fb027
Juan Luis Rosa Martinez <chiv0rz@gmail.com>
2022-12-07 15:05:30 +0100

    add d6 puzzle and solutions

---
+++ 2022/D6/python/day6.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import argparse
+from typing import List
+
+def part_one(data) -> List:
+    n = 4
+    for each in data:
+        marker = []
+        for idx, char in enumerate(each):
+            marker.append(char)
+            if len(set(marker[-n:])) == n:
+                print(idx+1)
+                break
+            
+def part_two(data) -> List:
+    n = 14
+    for each in data:
+        marker = []
+        for idx, char in enumerate(each):
+            marker.append(char)
+            if len(set(marker[-n:])) == n:
+                print(idx+1)
+                break
+
+def file_exist (string):
+    if not os.path.exists(string):
+        raise argparse.ArgumentTypeError('ERROR: input file does not exist.')
+    return string
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='D5 Input')
+    parser.add_argument('filename', metavar='FILE', type=file_exist,
+                    help='file doesnt exist')
+    
+    args = parser.parse_args() 
+    try:
+        with open(args.filename, "r") as file:
+            data = [x.strip() for x in file.readlines()]
+
+    except:
+        print("ERROR: input file is not readable.")
+        sys.exit(-1) 
+        
+    part_one(data)
+    part_two(data)
